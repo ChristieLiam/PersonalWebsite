82c2eafde1b10d6665971f2b36308fc1815a416f
lchristie <lm577319@dal.ca>
2025-10-22 17:26:03 -0300

    Lecture Activity 8 Submission

--- activities/lecture8/README.txt
+++ activities/lecture8/README.txt
@@ -1,7 +1,7 @@
 # Lecture 8 Activity
 
 * *Date Created*: 21 10 2025
-* *Last Modification Date*: 21 10 2025
+* *Last Modification Date*: 22 10 2025
 * *Lab URL*: <https://web.cs.dal.ca/~lchristie/csci3172/activities/lecture8/>
 * *Git URL*: <https://git.cs.dal.ca/lchristie/csci3172/activities/lecture8>
 
--- activities/lecture8/js/script.js
+++ activities/lecture8/js/script.js
@@ -3,39 +3,38 @@
 // You may explore the use of JS built-in functions
 
 
-
+const now = new Date();
+const time = now.toLocaleString();
+const hour = now.getHours();
 
 
 // Create a variable to store your greeting message 
 
+let greeting = "";
 
 
-// Based on the hour you get, you need to set the conditions you want your script to check
-// in order to render a specific message
-// for now we want to say 'Good Morning' if it is earlier than 12PM
+if (hour < 12) { // for now, we want to say 'Good Morning' if it is earlier than 12PM
 
+    greeting = "Good Morning";
 
+} else if (hour < 15) {// otherwise we want to check if it is earlier than 3PM and let the visitor know
 
-// otherwise we want to check if it is earlier than 3PM and let the visitor know
-// 'Hey! I think we are in class!'
+    greeting = "Hey! I think we are in class!"
 
+} else {
+    // For any other time (i.e., later than 3PM, we just want to say 'Welcome'
 
+    greeting = "Welcome"
 
-// For any other time (i.e., later than 3PM, we just want to say 'Welcome'
-
-
-
-
-// This is an example of an if statement, or a conditional statement
-// the JS interpreter checks if a conditions is true, if it is then it executes the code
-// If the condition is FALSE, then it skips the code and moves onto the next one (i.e., our else if conditional)
-// If that second condition is also FALSE then it moves to our last conditional, our else statement
-// IF statements always end in an ELSE statement, if you want to give options in-between we always use ELSE IF
-
-
+}
 
 // Then, we use the DOM, and calling the 'getElementById( )' method and its innerHTML property to add some HTML for us onto our webpage
 // we basically want to show the return result in <h2 id="greeting"></h2>
+
+document.getElementById("greeting").innerHTML = greeting;
+
+document.addEventListener("DOMContentLoaded", function() {})
+
 
 
 
@@ -44,6 +43,8 @@
 // and add them together
 // First we declare our variables for the two values
 
+let var1 = 0;
+let var2 = 0;
 
 // Now, let's use the DOM now to access a value in our form and show it back to us in an alert( ) box
 // First, we'll creatr a function to store the input values into the variables we declared
@@ -51,7 +52,21 @@
 
     // Store the values from the form into the variables we declared above
  
- 
+function getNumbers(){
+    var1 = document.getElementById("number1").value;
+    var2 = document.getElementById("number2").value;
+
+
+    if (isNaN(var1) || isNaN(var2)) {
+        alert("Please enter valid numbers in both fields.");
+        return null; // Return null to signal an error
+    }
+
+    var1 = parseInt(var1, 10);
+    var2 = parseInt(var2, 10);
+
+    alert("Number 1 is: "+var1+"    Number 2 is: "+var2);
+}
 
 
 
@@ -62,9 +77,51 @@
 	
 
 	// Display the result of the calculation
-	
 
 
+function addition(event){
+    event.preventDefault();
+
+    getNumbers();
+
+    let theAddition = var1 + var2;
+
+    document.getElementById("result-container").innerHTML = `<p>Result of Addition: ${theAddition.toLocaleString()}</p>`;
+
+}
+
+function subtract(event){
+    event.preventDefault();
+
+    getNumbers();
+
+    let theSubtraction = var1 - var2;
+
+    document.getElementById("result-container").innerHTML = `<p>Result of Subtraction: ${theSubtraction.toLocaleString()}</p>`;
 
 
+}
 
+function multiply(event){
+    event.preventDefault();
+
+    getNumbers();
+
+    let theMultiplication = var1 * var2;
+
+    document.getElementById("result-container").innerHTML = `<p>Result of Multiplaction: ${theMultiplication.toLocaleString()}</p>`;
+
+
+}
+
+function divide(event){
+    event.preventDefault();
+
+    getNumbers();
+
+    let theDivision = var1 / var2;
+
+    document.getElementById("result-container").innerHTML = `<p>Result of Division: ${theDivision.toLocaleString()}</p>`;
+
+
+}
