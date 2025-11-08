4fd4753c772fa6b06884c07058f4cbae04cc2965
lchristie <lm577319@dal.ca>
2025-11-08 19:55:57 -0400

    Added Lab 5 Bones

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function header() {
+    return (
+        <h4>Footer Here</h4>
+    );
+}
+
+export default header;
---
+++ src/pages/projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function projects() {
+    return (
+        <h4>Projects Here</h4>
+    );
+}
+
+export default projects;
